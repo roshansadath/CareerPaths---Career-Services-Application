5de027e903adb82be86ec5dd390a1dd6a752e433
Pankaj Deep <pankajdeepsahota@gmail.com>
2023-07-19 16:49:35 -0400

    #13, #14 Database Configuration and Base File

--- project/backend/index.js
+++ project/backend/index.js
@@ -1,1 +1,32 @@
-console.log('Career Service Application Under Construction')
+const express = require('express');
+const app = express();
+const sequelize=require('./config/database');
+const cors=require('cors');
+
+
+
+sequelize.sync({alter:true})
+   .then(()=>{
+      console.log('Database synchronized successfully.');
+   })
+   .catch((error)=>{
+      console.error('Unable to synchronized the database.',error);
+   });
+
+
+   //Middleware
+app.use(express.json());
+
+app.use(cors());
+
+app.use((err,req,res,next)=>{
+    console.error(err);
+    res.status(500).json({error:'Internal server error'});
+});
+
+//Start the server
+const port=7080;
+app.listen(port, () => {
+   console.log(`Server Running on port ${port}`);
+});
+
