2376d7005938601d15f492bd0896f0a2eca9a99a
manish198 <gautam.manish.manish@gmail.com>
2023-07-30 11:07:57 -0400

    Bug Fixes while apply for job

--- project/backend/controllers/userController.js
+++ project/backend/controllers/userController.js
@@ -1,18 +1,10 @@
-const multer = require('multer');
 const User= require('../model/user');
+const formidable = require('formidable');
+const fs=require('fs');
+const path=require('path');
+const { v4:uuidv4 }=require('uuid');
 
-//Setting up the storage for the files:
-const storage=multer.diskStorage({
-    destination:(req,file,cb)=>{
-        cb(null,'./uploads/');
-    },
-    filename:(req,file,cb)=>{
-        const uniqueSuffix=Date.now()+'-'+Math.round(Math.random()*1E9);
-        cb(null,'cv-'+ uniqueSuffix + path.extname(file.originalname));
-    },
-});
 
-const upload=multer({storage:storage}).single('cvFile');
 
 //To get user data that has logged in.
 module.exports.getProfile=async (req,res,next)=>{
@@ -47,12 +39,40 @@ module.exports.getUser=async (req,res,next)=>{
         next(error);
     }
 }
+// // Define a function to handle form data and file upload
+// function handleFormUpload(req, res) {
+//     const form = new formidable.IncomingForm();
+  
+//     // Parse the form data
+//     form.parse(req, (err, fields, files) => {
+//       if (err) {
+//         res.writeHead(400, { 'Content-Type': 'text/plain' });
+//         res.end('Error parsing the form data.');
+//         return;
+//       }
+//       const name = fields.name;
+//       console.log('Name:'+name);
+//       // Access the form fields and uploaded file information
+//       const uploadedFile = files.cvFile;
+//       console.log(uploadedFile.path);
+//       const fileName = uploadedFile[0].originalFilename;
+//       const fileSize = uploadedFile.size;
+//       const temporaryPath = uploadedFile.path;
+  
+//       // Here, you can process the uploaded file, move it to a desired location, etc.
+//       // For demonstration purposes, we'll just return a simple response with file information.
+//       res.writeHead(200, { 'Content-Type': 'text/plain' });
+//       res.write(`File name: ${fileName}\n`);
+//       res.write(`File size: ${fileSize} bytes\n`);
+//       res.write(`Temporary path: ${temporaryPath}\n`);
+//       res.end('File upload successful!');
+//     });
+//   }
 
 module.exports.createUser=async(req,res,next)=>{
-    try{
+    try{      
         const { username,password,name, email,userType }=req.body;
         console.log('Check:>>>>>>>>>>'+username+' '+password+' '+name+' '+email+' ');
-        
         //Create a newUser
         const newUser=await User.create({
             username,
@@ -62,28 +82,15 @@ module.exports.createUser=async(req,res,next)=>{
             userType,
             cvUrl:null,
         });
-        
-        //Handling CV upload if any.
-        upload(req,res,async(err)=>{
-            if(err){
-                console.error('Error uploading CV',err);
-                return res.json(newUser);
-            }
-            //If CV uploaded successfully
-            if(req.file){
-                newUser.cvUrl=req.file.path;
-                await newUser.save();
-            }
-
-            res.json(newUser);
-        });
-
-    }catch(error){
-        console.error('Error adding user:',error);
-        res.status(500).json({error:"Error occured while adding the user"});
-    }
-    
+        res.json({message:'User added successfully.'});
+     }catch(error){
+        console.error('Error adding user',error);
+        res.status(500).send('Error adding user.');
+     } 
 };
+
+  
+
 
 module.exports.updateUser=async(req,res,next)=>{
     try{
@@ -104,27 +111,16 @@ module.exports.updateUser=async(req,res,next)=>{
         user.email=email;
         user.userType=userType;
 
-        //Handle the CV update
-        upload(req,res,async(err)=>{
-            if (err){
-                console.error('Error uploading CV',err);
-                //Even if CV update fails update rest
-                await user.save();
-                return res.json(user);
-            }
-            //If CV uploaded successfulling change the cvUrl in the database.
-            if(req.file){
-                user.cvUrl=req.file.path;
-            }
-            //save the updated user to the database
-            await user.save();
-            res.json(user);
-        });
+        await user.save();
+        res.json({message:'User updated successfully.'});
+        
 
     }catch(error){
         console.error('Error updating a user',error);
         res.status(500).json({error:'Error occured while updating a user'})
     }
+
+    
     
 };
 
