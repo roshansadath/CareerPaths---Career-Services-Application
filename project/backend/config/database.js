df4a722cc37055df073b6db16fa08339bb4a5841
manish198 <gautam.manish.manish@gmail.com>
2023-08-01 10:14:34 -0400

    Dialect changed.

--- project/backend/config/database.js
+++ project/backend/config/database.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 const sequelize=new Sequelize(process.env.DB_NAME,process.env.DB_USER,process.env.DB_PASSWORD,{
     host:process.env.DB_HOST,
-    dialect:process.env.DB_TYPE,
+    dialect:'mysql',
     define: {
         timestamps: false, // Disable the timestamps feature globally
     },
