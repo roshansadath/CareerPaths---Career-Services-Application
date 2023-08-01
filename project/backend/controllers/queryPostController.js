2fedf92c362f0b5fc52956894a0c7f38712feac7
manish198 <gautam.manish.manish@gmail.com>
2023-08-01 18:33:31 -0400

    function to get single query post added and the route for it

--- project/backend/controllers/queryPostController.js
+++ project/backend/controllers/queryPostController.js
@@ -22,3 +22,16 @@ module.exports.createQueryPost=async(req,res,next)=>{
         res.status(500).json({error:"Error occured while adding query post"});
     }
 };
+
+module.exports.getQueryPost=async(req,res,next)=>{
+    const{ id }=req.params;
+    try{
+        const queryPost=await QueryPost.findByPk(id);
+        if(!queryPost){
+            return res.status(404).json({error:'Query Post not found.'});
+        }
+        res.json(queryPost);
+    }catch(error){
+        next(error);
+    }
+}
--- project/backend/routes/queryPostRoutes.js
+++ project/backend/routes/queryPostRoutes.js
@@ -4,5 +4,6 @@ const {authenticateToken}=require('../middleware/jwt');
 const queryPostController=require('../controllers/queryPostController');
 
 router.post('/',authenticateToken,queryPostController.createQueryPost);
+router.get('/:id',queryPostController.getQueryPost);
 
 module.exports=router;
