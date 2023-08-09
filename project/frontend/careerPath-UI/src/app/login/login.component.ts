0ca4373ea2de0987230e10d5f18ce2c50883fd88
lakshyakalia <lkalia.cse16@chitkarauniversity.edu.in>
2023-08-08 22:12:03 -0400

    Added comments

--- project/frontend/careerPath-UI/src/app/login/login.component.ts
+++ project/frontend/careerPath-UI/src/app/login/login.component.ts
@@ -10,44 +10,66 @@ import { MatSnackBar } from '@angular/material/snack-bar';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  // Property to track loading state
   isLoading: boolean = false;
+
+  // Form group for the login form
   loginForm: FormGroup;
   
-  constructor(private loginService: LoginService,
+  constructor(
+    private loginService: LoginService,
     private formBuilder: FormBuilder,
     private router: Router,
-    private snackBar: MatSnackBar){
-      this.loginForm = this.formBuilder.group({
-        username: '',
-        password: '',
-      });
-    }
-
-
-  ngOnInit(): void{
-
+    private snackBar: MatSnackBar
+  ) {
+    // Create the login form using FormBuilder
+    this.loginForm = this.formBuilder.group({
+      username: '',
+      password: '',
+    });
   }
 
-  submitLoginData(): void{
-    // let data = '';
+  ngOnInit(): void {
+    // Component initialization logic
+  }
+
+  // Submit login data to the server
+  submitLoginData(): void {
+    // Output the form values to the console (for testing)
     console.log(this.loginForm.value);
+
+    // Set loading state to true
     this.isLoading = true;
-    this.loginService.sendLoginData(this.loginForm.value)
-    .subscribe({
-      next: response=> {
+
+    // Send login data to the service and subscribe to the response
+    this.loginService.sendLoginData(this.loginForm.value).subscribe({
+      next: response => {
+        // On successful login response
         this.isLoading = false;
+
+        // Store the user token in local storage
         localStorage.setItem('userToken', response.token);
+
+        // Emit changes to notify other components
         this.emitChanges();
+
+        // Navigate to the profile page
         this.router.navigate(['/profile']);
-      }, error: err => {
-      console.log(err);
-      this.isLoading = false;
-      this.snackBar.open('Username or password incorrect!', 'Dismiss', {
-        duration: 2000, // Set the duration (in milliseconds) for how long the snackbar will be displayed
-      });
-    }
+      },
+      error: err => {
+        // On login error
+        console.log(err);
+        this.isLoading = false;
+
+        // Display a snackbar with an error message
+        this.snackBar.open('Username or password incorrect!', 'Dismiss', {
+          duration: 2000,
+        });
+      }
     });  
   }
+
+  // Emit changes using the login service
   emitChanges() {
     this.loginService.notifyChanges();
   }
--- project/frontend/careerPath-UI/src/app/new-job-posting/new-job-posting.component.ts
+++ project/frontend/careerPath-UI/src/app/new-job-posting/new-job-posting.component.ts
@@ -9,52 +9,67 @@ import { MatSnackBar } from '@angular/material/snack-bar';
   styleUrls: ['./new-job-posting.component.css']
 })
 export class NewJobPostingComponent {
-
+  // Form group for the job posting form
   jobPostingForm: FormGroup;
+  // Property to track loading state
   isLoading: boolean = false;
 
-  constructor(private formBuilder: FormBuilder,
+  constructor(
+    private formBuilder: FormBuilder,
     private employeeService: EmployerService,
-    private snackBar: MatSnackBar){
+    private snackBar: MatSnackBar
+  ) {
+    // Create the job posting form using FormBuilder
     this.jobPostingForm = this.formBuilder.group({
       jobTitle: '',
       jobDesc: '',
       nameOfTeam: '',
       payRange: '',
       location: ''
-    })
+    });
   }
 
-  ngOnInit(): void{
-
+  ngOnInit(): void {
+    // Component initialization logic
   }
 
-  submitNewFormData(): void{
+  // Submit new job posting data to the server
+  submitNewFormData(): void {
+    // Set loading state to true
     this.isLoading = true;
+
+    // Output the form values to the console (for testing)
     console.log(this.jobPostingForm.value);
+
+    // Prepare data for new job posting
     let data = {
       job_designation: this.jobPostingForm.value.jobTitle,
       job_description: this.jobPostingForm.value.jobDesc,
       location: this.jobPostingForm.value.location,
       name_of_the_team: this.jobPostingForm.value.nameOfTeam,
       pay_range: this.jobPostingForm.value.payRange
-    }
-    this.employeeService.postNewJobData(data)
-    .subscribe({
-      next: response=> {
+    };
+
+    // Send new job posting data to the service and subscribe to the response
+    this.employeeService.postNewJobData(data).subscribe({
+      next: response => {
+        // On successful job posting response
         this.isLoading = false;
-        // if(response.statusCode == 200){
-          this.snackBar.open("Job Posting Successful!", 'Dismiss', {
-            duration: 2000, // Set the duration (in milliseconds) for how long the snackbar will be displayed
-          });
-        // }
-      }, error: err => {
-      // console.log(err);
-      this.isLoading = false;
-      this.snackBar.open(err, 'Dismiss', {
-        duration: 2000, // Set the duration (in milliseconds) for how long the snackbar will be displayed
-      });
-    }
+
+        // Display a snackbar with a success message
+        this.snackBar.open('Job Posting Successful!', 'Dismiss', {
+          duration: 2000,
+        });
+      },
+      error: err => {
+        // On job posting error
+        this.isLoading = false;
+
+        // Display a snackbar with an error message
+        this.snackBar.open(err, 'Dismiss', {
+          duration: 2000,
+        });
+      }
     });
   }
 }
